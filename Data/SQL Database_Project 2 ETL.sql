fd45765a35085a03b2f93bd39cbac245c4519f85
obrien630 <obrien630@gmail.com>
2019-03-03 16:54:12 -0500

    Updating project

---
+++ Data/SQL Database_Project 2 ETL.sql
@@ -0,0 +1,11 @@
+-- Create Database to import csv into
+CREATE DATABASE School_populations_and_shootings;
+
+USE school_populations_and_shootings;
+
+SELECT * FROM census_school_populations;
+
+SELECT * FROM school_shootings;
+
+ALTER TABLE school_shootings
+ADD FOREIGN KEY (Year) REFERENCES census_school_populations(Year);
